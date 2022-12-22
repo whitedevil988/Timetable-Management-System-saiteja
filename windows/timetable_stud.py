7736ca1abff96f40a8a173d251426e4bb632db15
Rohit <kasamrohitreddy@gmail.com>
2022-12-22 14:10:12 +0530

    periods

--- windows/timetable_stud.py
+++ windows/timetable_stud.py
@@ -3,15 +3,15 @@ from tkinter import ttk
 from tkinter import messagebox
 import sqlite3
 
-days = 5
-periods = 6
-recess_break_aft = 3 # recess after 3rd Period
+days = 6
+periods = 7
+recess_break_aft = 4 # recess after 3rd Period
 section = None
 butt_grid = []
 
 
-period_names = list(map(lambda x: 'Period ' + str(x), range(1, 6+1)))
-day_names = ['Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday']
+period_names = list(map(lambda x: 'Period ' + str(x), range(1, 7+1)))
+day_names = ['Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday']
 
 
 
